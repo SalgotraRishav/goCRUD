fad1da99d8ffdbb445d7b3ade919bc841564bcab
SalgotraRishav <salgotra_rishav@yahoo.in>
2024-02-15 17:26:58 -0500

    Adding main.go

---
+++ go.mod
@@ -0,0 +1,3 @@
+module GOCRUD
+
+go 1.21.6
---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+// task represent a to-do task.
+type Task struct {
+	ID     string `json:"id"`
+	Title  string `json:"title"`
+	Status string `json:"status"`
+}
+
+var tasks = []Task{}
+
+const Dport = ":8012"
+
+func main() {
+
+}
